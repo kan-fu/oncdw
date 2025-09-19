60245f888466e03bc498fdb28f611481e6a786d5
Kan Fu <kanfu@uvic.ca>
2025-09-18 17:13:32 -0700

    data elements

--- Home.py
+++ Home.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import os
+from onc import ONC
+import pandas as pd
 
 st.title("Oceans 3.0 Open API Playground")
+st.metric("Web Services", 2, 1)
 
+token = os.getenv("ONC_TOKEN")
+onc = ONC(token)
 
 st.header(":blue[Discovery Services]")
 
@@ -12,7 +18,9 @@ st.markdown(":blue-badge[GET] `/locations`")
 location_code = st.text_input("locationCode", placeholder="FGPD")
 
 if st.button("Run", key="location_button"):
-    st.write(f"you input {location_code}")
+    param = {"locationCode": location_code}
+    location_info = onc.getLocations(param)
+    st.json(location_info)
 
 st.divider()
 
@@ -26,5 +34,10 @@ device_code = st.text_input("deviceCode", value="BPR_BC")
 last_days = st.number_input("last days", value=4)
 
 if st.button("Run", key="archivefile_button"):
-    st.write(f"you input {device_code}")
-    st.write(f"you input {last_days}")
+    param = {
+        "deviceCode": device_code,
+        "dateFrom": f"-P{last_days}D",
+        "returnOptions": "all",
+    }
+    archivefile = onc.getArchivefile(param)
+    st.dataframe(pd.DataFrame(archivefile["files"]))
